b02bd9808b57e5fb155532ab87093f01ed11a31d
Ranajit418 <ranajit418@gmail.com>
2021-12-15 16:07:22 +0530

    fifth commit

--- rolebase/settings.py
+++ rolebase/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATES_DIR=os.path.join(BASE_DIR/'template')
+TEMPLATE_DIR=os.path.join(BASE_DIR/'template')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 # Quick-start development settings - unsuitable for production
